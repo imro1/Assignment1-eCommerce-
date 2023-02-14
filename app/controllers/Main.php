d9660b1144356a26fc65261751fdf3faddaca3a5
Aliraza1002 <Aliraza_159@outlook.com>
2023-02-14 15:03:16 -0500

    made some changes

--- app/controllers/Main.php
+++ app/controllers/Main.php
@@ -3,7 +3,6 @@ namespace app\controllers;
 
 class Main extends \app\core\Controller{
 
-	private static $file = 'resources/counter.txt';
 	//main index view
 	function index(){
 		$this->view('Main/index');
@@ -11,30 +10,5 @@ class Main extends \app\core\Controller{
 	//abous us view
 	function about_us(){
 		$this->view('Main/about_us');
-	}
-
-	public function counter(){
-		if(file_exists(self)) 
-		{
-			$fh = fopen(self::$file, 'r');
-			flock($fh, LOCK_EX);
-			$counter = fread($fh, filesize(self::$file));
-			flock($fh, LOCK_UN);
-			fclose($fh);
-		}
-		else
-		{
-			$counter = '{"count":0}';
-		}
-
-		$updateCounter = json_decode($counter, true);
-		$updateCounter['count']++;
-		$counter = json_encode($updateCounter);
-		echo $counter;
-		$fh = fopen(self::$file, 'w');
-		flock($fh, LOCK_EX);
-		fwrite($fh, $counter);
-		flock($fh, LOCK_UN);
-		fclose($fh);
 	}
 }
--- app/views/shared/footer.php
+++ app/views/shared/footer.php
@@ -1,2 +1,43 @@
+<style>
+	ul {
+		display:inline;
+		margin: 0;
+		padding: 0;
+		width: 200px;
+		background-color: #f1f1f1;
+		border: 1px solid #555;
+		}
+
+		li a {
+		display: block;
+		color: #000;
+		padding: 10px;
+		text-decoration: none;
+		}
+
+		li {
+		text-align: center;
+		border-bottom: 1px solid #555;
+		}
+
+		li:last-child {
+		border-bottom: none;
+		}
+
+		li a.active {
+		background-color: #04AA6D;
+		color: white;
+		}
+
+		li a:hover:not(.active) {
+		background-color: #555;
+		color: white;
+		}
+
+		.main{
+			padding-right: -100%;
+			text-align: center;
+		}
+</style>
 </body>
 </html>
